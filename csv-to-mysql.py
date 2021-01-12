001fe6efeeb10ce5fae62e8e751e2ad1ad8554f9
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-11 19:32:26 -0500

    added supplementary functions and reformatted execute calls

--- csv-to-mysql.py
+++ csv-to-mysql.py
@@ -3,6 +3,10 @@ import MySQLdb
 
 # installing MySQL: https://dev.mysql.com/doc/refman/8.0/en/osx-installation-pkg.html
 # how to start, watch: https://www.youtube.com/watch?v=3vsC05rxZ8c
+# or read this (absolutely helpful) guide: https://www.datacamp.com/community/tutorials/mysql-python
+
+# this is mainly created to get a database of all the songs in my Favorites playlist
+# if you wish to change the topic to 'FILM', 'SPORTS', or 'POLITICS'
 
 # 1/ initially, set up the MySQL connection and craft a cursor
 # mydb = MySQLdb.connect(host='localhost', user='root', passwd='yourPasswordHere')
@@ -18,12 +22,12 @@ cursor = mydb.cursor()
 
 # from here on out, whenever you call `cursor.execute()`, call `mydb.commit()` right afterwards
 
-# 4/ create a table
+# 4/ create a table -- three options available to you
 # the table's hardcoded right now so if the columns here are changed then other
-def create_table(table_name):
+def initialise_main_music_table(table_name):
     cursor.execute("CREATE TABLE " + table_name
                     + " (songID INTEGER PRIMARY KEY AUTO_INCREMENT, \
-                    songTitle VARCHAR(100) NOT NULL, \
+                    songTitle VARCHAR(150) NOT NULL, \
                     artist VARCHAR(100) NOT NULL, \
                     genre VARCHAR(100) NOT NULL, \
                     videoLink VARCHAR(100) NOT NULL, \
@@ -31,21 +35,30 @@ def create_table(table_name):
                     likeToDislikeRatio decimal(5, 4) NOT NULL)")
     mydb.commit()
 
-# just an example; don't uncomment;
-# cursor.execute(
-#             'INSERT INTO Music (songTitle, artist, genre, videoLink, likeToDislikeRatio) \
-#             VALUES("%s", "%s", "%s", "%s", "%s")', \
-#             ('Eat Them Apples', "Suzi Wu-Topic", 'INDEPENDENT MUSIC', "https://www.youtube.com/watch?v=Bs7lVqYBqys", 0.9710))
-
-# 4.1/ if you want to add a field after table is created (new field placed after a specific column of a table)
-def add_new_column(table_name, new_column_name, data_type, pivot_column):
-    cursor.execute("ALTER TABLE " + table_name + " ADD " + new_column_name + " " + data_type + " NOT NULL AFTER " + pivot_column)
+# the main music table helps extract info to create sub tables for a specific music category
+def initialise_custom_music_table(table_name, main_music_table_name):
+    cursor.execute("CREATE TABLE " + table_name
+                    + " (categorySongID INTEGER PRIMARY KEY AUTO_INCREMENT, \
+                    mainSongID INTEGER NOT NULL DEFAULT 1, \
+                    FOREIGN KEY(mainSongID) REFERENCES " + main_music_table_name + "(songID), \
+                    songTitle VARCHAR(150) NOT NULL, \
+                    artist VARCHAR(100) NOT NULL, \
+                    genre VARCHAR(100) NOT NULL, \
+                    videoLink VARCHAR(100) NOT NULL, \
+                    viewCount BIGINT NOT NULL, \
+                    likeToDislikeRatio decimal(5, 4) NOT NULL)")
     mydb.commit()
 
-# 4.2/ if you want to change data type for any given field
-def modify_data_type(table_name, column_name, new_data_type):
-    cursor.execute("ALTER TABLE " + table_name + " MODIFY COLUMN " + column_name + " " + new_data_type)
-    mydb.commit()
+# def create_custom_table(table_name):
+#     cursor.execute("CREATE TABLE " + table_name
+#                     + " (tableID INTEGER PRIMARY KEY AUTO_INCREMENT, \
+#                     videoTitle VARCHAR(150) NOT NULL, \
+#                     author VARCHAR(100) NOT NULL, \
+#                     category VARCHAR(100) NOT NULL, \
+#                     videoLink VARCHAR(100) NOT NULL, \
+#                     viewCount BIGINT NOT NULL, \
+#                     likeToDislikeRatio decimal(5, 4) NOT NULL)")
+#     mydb.commit()
 
 # 5/ from a list of wanted fields, the function searches for the index corresponding to each field on the list
 # and stores the index inside a dict (easy to look up and flexible if the order of the columns in the csv file is changed)
@@ -57,28 +70,13 @@ def get_indices_of_csv_table_items(csv_file_name, wanted_items):
         for idx, heading in enumerate(csv_headings):
             if heading in wanted_items:
                 indices[heading] = idx
+    csv_file.close()
     return indices
 
-# optional / not required function: should you wish to look up the different video topics
-# if you want to search for all topics, leave `selected_topic` as an empty string
-def get_all_selected_topics(csv_file_name, selected_topic):
-    res = []
-    wanted_items = ['song_name', 'artist', 'topics', 'video_link', 'view_count', 'like_to_dislike_ratio']
-    indices = get_indices_of_csv_table_items(csv_file_name, wanted_items)
-    with open(csv_file_name) as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        csv_headings = next(csv_data)
-        for idx, row in enumerate(csv_data):
-            topics = row[indices['topics']][1:-1]
-            topic_list = topics.split(', ')
-            for item in topic_list:
-                if selected_topic in item and item not in res:
-                    res.append(item)
-    return res
+wanted_items = ['song_name', 'artist', 'topics', 'video_link', 'view_count', 'like_to_dislike_ratio']
 
-# 6/ fill up our table with the relevant data
-def populate_table_from_csv(csv_file_name, table_name, selected_topic):
-    wanted_items = ['song_name', 'artist', 'topics', 'video_link', 'view_count', 'like_to_dislike_ratio']
+# 6/ fill up our main table with the relevant data
+def populate_main_music_table_from_csv(csv_file_name, table_name):
     indices = get_indices_of_csv_table_items(csv_file_name, wanted_items)
     with open(csv_file_name) as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
@@ -95,35 +93,94 @@ def populate_table_from_csv(csv_file_name, table_name, selected_topic):
             if row[indices['like_to_dislike_ratio']]:
                 ratio = float(row[indices['like_to_dislike_ratio']][:-1]) / 100
 
-            if selected_topic in genre:
-                cursor.execute('INSERT INTO ' + table_name + \
-                                ' (songTitle, artist, genre, videoLink, viewCount, likeToDislikeRatio) \
-                                VALUES("%s", "%s", "%s", "%s", "%s", "%s")', \
-                                (song_name, artist, genre, video_link, view_count, ratio))
+            if 'MUSIC' in genre:
+                cursor.execute(f"INSERT INTO {table_name} (songTitle, artist, genre, videoLink, viewCount, likeToDislikeRatio)\
+                                VALUES(%s, %s, %s, %s, %s, %s)", (song_name, artist, genre, video_link, view_count, ratio))
+    mydb.commit() # remember to commit after populating the table
+    csv_file.close()
 
-# delete the entire table
-def delete_whole_table(table_name):
-    cursor.execute('DELETE FROM ' + table_name)
+# 7/ fill up our custom table using data from the main music table
+def populate_custom_music_table(table_name, main_music_table_name, column, chosen_value):
+    cursor.execute(f"INSERT INTO {table_name} (mainSongID, songTitle, artist, genre, videoLink, viewCount, likeToDislikeRatio)\
+                    SELECT songID, songTitle, artist, genre, videoLink, viewCount, likeToDislikeRatio \
+                    FROM {main_music_table_name} WHERE {column} LIKE '%{chosen_value}%'")
+    mydb.commit()
+
+# -------------------------------------------------------------------
+# -------------------SUPPLEMENTARY FUNCTIONS START-------------------
+# -------------------------------------------------------------------
+
+# add a field after table is created (new field placed after a specific column of a table)
+def add_new_column(table_name, new_column_name, data_type, pivot_column):
+    cursor.execute(f"ALTER TABLE {table_name} ADD {new_column_name} {data_type} NOT NULL AFTER {pivot_column}")
+    mydb.commit()
+
+# change data type for any given field
+def modify_data_type(table_name, column_name, new_data_type):
+    cursor.execute(f"ALTER TABLE {table_name} MODIFY COLUMN {column_name} {new_data_type}")
+    mydb.commit()
+
+# delete all the data from a specified table
+def delete_data_from_table(table_name):
+    cursor.execute(f"DELETE FROM {table_name}")
     mydb.commit()
 
 def delete_selected_record_from_table(table_name, record):
-    cursor.execute('DELETE FROM ' + table_name + ' WHERE address = ' + record)
+    cursor.execute(f"DELETE FROM {table_name} WHERE address = {record}")
     mydb.commit()
 
+# make a table disappear from existence :)
+def drop_table(table_name):
+    cursor.execute(f"DROP TABLE {table_name}")
+    mydb.commit()
 
-# print out all the songs in the playlist (most popular on top)
-def print_items(table_name):
-    cursor.execute("SELECT * FROM " + table_name + " ORDER BY viewCount DESC")
-    for item in cursor:
-        print(item)
+def print_table_plain(table_name):
+    cursor.execute(f"SELECT * FROM {table_name}")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
 
-def main():
-    table_name = 'IndieMusic'
-    selected_topic = 'INDEPENDENT MUSIC'
-    csv_file_name = 'favorite-playlist.csv'
-    create_table(table_name)
-    populate_table_from_csv(csv_file_name, table_name, selected_topic)
-    print_items(table_name)
+# print out all the songs in the playlist
+# 'DESC' means descending order (most popular song on top) and 'ASC' is the opposite
+def print_table_by_criteria(table_name, order_criteria, order):
+    if order_criteria != '' and order != '':
+        cursor.execute(f"SELECT * FROM {table_name} ORDER BY {order_criteria} {order}")
+        for item in cursor:
+            print(item)
 
-main()
-# print_items('Music')
+# show the name of all the tables present in the database
+def show_tables():
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    print(tables)
+
+# check if a table already exists
+def check_table_exists(table_name):
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = '{0}'
+        """.format(table_name.replace('\'', '\'\'')))
+    return True if cursor.fetchone()[0] == 1 else False
+
+# optional / not required function: should you wish to look up the different video topics
+# if you want to search for all topics, leave `selected_topic` as an empty string
+def get_all_selected_topics(csv_file_name, selected_topic):
+    res = []
+    indices = get_indices_of_csv_table_items(csv_file_name, wanted_items)
+    with open(csv_file_name) as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        csv_headings = next(csv_data)
+        for idx, row in enumerate(csv_data):
+            topics = row[indices['topics']][1:-1]
+            topic_list = topics.split(', ')
+            for item in topic_list:
+                if selected_topic in item and item not in res:
+                    res.append(item)
+    return res
+
+# ------------------------------------------------------------------
+# -------------------SUPPLEMENTARY FUNCTIONS ENDS-------------------
+# ------------------------------------------------------------------
+
+
